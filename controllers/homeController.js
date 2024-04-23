2dc96b393a5fbc34745271cd246b2ee6e681cbab
2026074 조형진 <163215389+jhjiin@users.noreply.github.com>
2024-04-23 22:46:18 +0000

    7끝

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,12 +6,15 @@
  * 홈 컨트롤러로의 라우팅
  */
 exports.showCourses = (req, res) => {
-    res.render("courses");
+    res.render("courese");
+};
+exports.getHome = (req, res) => {
+    res.render("index");
 };
 exports.showSignUp = (req, res) => {
     res.render("contact");
 };
-exports.showCourses = (req, res) => {
+exports.postSignupForm = (req, res) => {
     res.render("thanks");
 };
 
@@ -22,3 +25,23 @@ exports.showCourses = (req, res) => {
  * [노트] 뷰 안에서는 offeredCourses라는 변수명을 통해 배열에 접근할 수 있다.
  * 홈 컨트롤러 내에서 이 배열은 courses라는 이름으로 다룬다.
  */
+var courses = [
+    {
+        title: "Event Driven Cakes",
+        cost: 50
+    },
+    {
+        title: "Asynchronous Artichoke",
+        cost: 25
+    },
+    {
+        title: "Object Oriented Orange Juice",
+        cost: 10
+    }
+];
+
+exports.showCourses = (req, res) => {
+    res.render("courses",{
+    offeredCourses: courses
+    });
+};
--- main.js
+++ main.js
@@ -2,17 +2,16 @@
 // Capstone 2: Express
 "use strict";
 
-const { application } = require("express");
-const express = require("express"),
-    app = express();
-
-
 // 앱 설정
-app.set("port", process.env.PORT || 3000);
+const express = require("express"),
+app = express(),
+homeController = require("./controllers/homeController"),
+errorController = require("./controllers/errorController");
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Confetti Cusine!");
-})
+app.set("port", process.env.PORT || 3000);
+app.set("/", (req, res) => {
+    res.send("Welcome to Confetti Cuisine!");
+});
 
 /**
  * Listing 12.7 (p. 179)
@@ -20,13 +19,13 @@ app.get("/", (req, res) => {
  */
 const layouts = require("express-ejs-layouts");
 
-app.set("view engine, ejs");
+app.set("view engine", "ejs");
 app.use(layouts);
-
 /**
  * Listing 12.4 (p. 177)
  * body-parser의 추가
  */
+
 app.use(
     express.urlencoded({
         extended: false
@@ -34,26 +33,23 @@ app.use(
 );
 app.use(express.json());
 
-
 /**
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
+app.get("/", homeController.getHome);
 app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSighUp);
-app.get("/contact", homeController.showSignUpForm);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postSignupForm);
 
 /**
  * Listing 12.12 (p. 184)
  * 에러 처리 라우트 
  */
-
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 // 3000번 포트로 리스닝 설정
 app.listen(app.get("port"), () => {
-    console.log(
-        `Server running at http://localhost:${app.get(
-            "port"
-        )}`
-    );
+    console.log(`Server Running at http://localhost:${app.get("port")}`);
 });
